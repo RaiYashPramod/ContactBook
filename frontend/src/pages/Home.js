512445a48ab2e8a6cd4064c37dacc9312a1dd42b
RaiYashPramod <raiyashpramod16@gmail.com>
2022-09-08 19:23:47 +0530

    alts

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -38,7 +38,7 @@ const Home = () => {
         setEmail(contact.email);
     }
 
-    const updateContact = async(e,contacts) => {
+    const updateContact = async(e) => {
         e.preventDefault();
 
         const response = await fetch('http://localhost:5000/api/contacts/', {
